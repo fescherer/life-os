f0b627033550c62fa0e317ec35a3b29cb2ed13be
unknown <felipescherer25@gmail.com>
2025-08-11 21:28:04 -0300

    :sparkles: add log file

--- src/types/util.ts
+++ src/types/util.ts
@@ -2,3 +2,13 @@ export type TValidate = {
     isValid: boolean
     missingFields: string[]
 }
+
+export type TLogType = '⚠️' | '❌' | '✅' | '📝'
+
+export type TLog = {
+    id: number,
+    date: string,
+    type: TLogType,
+    message: string,
+
+}
